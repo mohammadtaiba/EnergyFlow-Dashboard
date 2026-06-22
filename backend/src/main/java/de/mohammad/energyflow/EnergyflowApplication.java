4e2ba860aa62645107abd5fe3d95f4eaab9aeda3
mohammad taiba <mohammadtaiba54@gmail.com>
2026-06-22 12:23:23 +0200

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/de/mohammad/energyflow/EnergyflowApplication.java
@@ -0,0 +1,13 @@
+package de.mohammad.energyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/mohammad/energyflow/EnergyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mohammad.energyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
